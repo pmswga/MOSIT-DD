156658b763d2b7fd506bc630447b2b03776304bd
Sergey <pmswga@gmail.com>
2020-05-13 18:42:56 +0300

    Update web.php

--- mdd/routes/web.php
+++ mdd/routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 \Illuminate\Support\Facades\Auth::routes();
 
 
-Route::get('/', "MainPageController@index")->name('index');
-Route::get('/login', "MainPageController@login")->name('login');
-Route::get('/about', "MainPageController@about")->name('about');
+Route::get('/', 'MainPageController@index')->name('index');
+Route::get('/login', ['middleware' => 'guest','uses' => 'MainPageController@login'])->name('login');
+Route::get('/about', 'MainPageController@about')->name('about');
 
 Route::get('/home', ['middleware' => 'auth', 'uses' => 'Services\Accounts\AccountPageController@home'])->name('home');
 Route::get('/profile', ['middleware' => 'auth', 'uses' => 'Services\Accounts\AccountPageController@profile'] )->name('profile');
