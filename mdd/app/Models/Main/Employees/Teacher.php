60292fe536e1e93a908e63a7479cddc1e09cb55d
Sergey <pmswga@gmail.com>
2020-05-14 22:27:32 +0300

    Update Teacher.php

--- mdd/app/Models/Main/Employees/Teacher.php
+++ mdd/app/Models/Main/Employees/Teacher.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Main\Employees;
 
+use App\Models\Main\IP\IP;
 use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model
@@ -9,5 +10,15 @@ class Teacher extends Model
     protected $table = 'Teachers';
     protected $primaryKey = 'idTeacher';
     public $timestamps = false;
+
+    public function getIPS() {
+        $ips = $this->hasOne(IP::class,'idTeacher', 'idTeacher')->get();
+
+        if ($ips) {
+            return $ips;
+        }
+
+        return null;
+    }
 
 }
