2014c0d0bcc0119dc4e2b41029efa1d75c65c521
Sergey <pmswga@gmail.com>
2020-05-14 22:27:30 +0300

    Update IP.php

--- mdd/app/Models/Main/IP/IP.php
+++ mdd/app/Models/Main/IP/IP.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\Main\IP;
 
+use App\Models\Main\Employees\Employee;
+use App\Models\Main\Employees\Teacher;
 use Illuminate\Database\Eloquent\Model;
 
 class IP extends Model
@@ -9,5 +11,20 @@ class IP extends Model
     protected $table = 'ips';
     protected $primaryKey = 'idIP';
     public $timestamps = false;
+
+    public function getTeacherInitials() {
+        $teacher = $this->hasOne(Teacher::class, 'idTeacher', 'idTeacher')
+                ->where('idTeacher', '=', $this->idTeacher)->get()->first();
+
+        if ($teacher) {
+            $employee = $teacher->hasOne(Employee::class, 'idEmployee', 'idEmployee')->get()->first();
+
+            if ($employee) {
+                return $employee->getFullInitials();
+            }
+        }
+
+        return '';
+    }
 
 }
