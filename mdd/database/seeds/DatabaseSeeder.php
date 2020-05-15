1548fcd47e8eb4772ee8ce10fb59417c5efd34e5
Sergey <pmswga@gmail.com>
2020-05-15 19:05:56 +0300

    Рефакторинг сеялок

--- mdd/database/seeds/DatabaseSeeder.php
+++ mdd/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ListAccountType::class,
+            ListAccountTypeSeeder::class,
             ListEmployeePost::class,
             ListTeacherPost::class,
 
--- mdd/database/seeds/Service/Lists/ListAccountTypeSeeder.php
+++ mdd/database/seeds/Service/Lists/ListAccountTypeSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ListAccountType extends Seeder
+class ListAccountTypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
