0bfbbfc9be28d434d9f51b6d2fb5042b412d2148
Sergey <pmswga@gmail.com>
2020-05-24 13:18:46 +0300

    Создание модели и ресурсного контроллера для поручений

---
+++ mdd/app/Http/Controllers/Main/Tickets/TicketResourceController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Main\Tickets;
+
+use App\Http\Controllers\Controller;
+use App\Models\Main\Tickets\TicketModel;
+use Illuminate\Http\Request;
+
+class TicketResourceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Main\Tickets\TicketModel  $ticketModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TicketModel $ticketModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Main\Tickets\TicketModel  $ticketModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TicketModel $ticketModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Main\Tickets\TicketModel  $ticketModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TicketModel $ticketModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Main\Tickets\TicketModel  $ticketModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TicketModel $ticketModel)
+    {
+        //
+    }
+}
---
+++ mdd/app/Models/Main/Tickets/TicketModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Main\Tickets;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TicketModel extends Model
+{
+    protected $table = 'tickets';
+    protected $primaryKey = 'idTicket';
+
+
+
+}
