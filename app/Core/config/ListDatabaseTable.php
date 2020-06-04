7d8c1ce02f1de8ead36132bf8be8399a123a0c8a
Sergey <pmswga@gmail.com>
2020-06-04 16:18:24 +0300

    Update ListDatabaseTable.php

--- app/Core/config/ListDatabaseTable.php
+++ app/Core/config/ListDatabaseTable.php
@@ -9,37 +9,37 @@ class ListDatabaseTable
 
     public const TABLE_FAILED_JOBS = 'Failed_jobs';
 
-    public const TABLE_ACCOUNTS = 'Accounts';
-    public const TABLE_PASSWORD_RESETS = 'Password_reset';
-    public const TABLE_LIST_SYSTEM_SECTION = 'List_system_section';
-    public const TABLE_LIST_SUB_SYSTEM = 'List_sub_system';
-    public const TABLE_ACCOUNT_RIGHTS = 'Account_rights';
-    public const TABLE_LIST_ACCOUNT_TYPE = 'List_account_type';
-    public const TABLE_LIST_FACULTY = 'List_faculty';
-    public const TABLE_LIST_INSTITUTE = 'List_institute';
+    public const TABLE_ACCOUNTS = 'accounts';
+    public const TABLE_PASSWORD_RESETS = 'password_reset';
+    public const TABLE_LIST_SYSTEM_SECTION = 'list_system_section';
+    public const TABLE_LIST_SUB_SYSTEM = 'list_sub_system';
+    public const TABLE_ACCOUNT_RIGHTS = 'account_rights';
+    public const TABLE_LIST_ACCOUNT_TYPE = 'list_account_type';
+    public const TABLE_LIST_FACULTY = 'list_faculty';
+    public const TABLE_LIST_INSTITUTE = 'list_institute';
 
-    public const TABLE_IPS = 'Ips';
+    public const TABLE_IPS = 'ips';
 
-    public const TABLE_EMPLOYEES = 'Employees';
-    public const TABLE_TEACHERS = 'Teachers';
-    public const TABLE_LIST_DEGREE = 'List_degree';
-    public const TABLE_LIST_ACADEMIC_TITLE = 'List_academic_title';
-    public const TABLE_LIST_SCIENCE_TYPE = 'List_science_type';
-    public const TABLE_LIST_TEACHER_POST = 'List_teacher_post';
-    public const TABLE_LIST_EMPLOYEE_POST = 'List_employee_post';
-    public const TABLE_RATES = 'Rates';
-    public const TABLE_LIST_RATE_TYPE = 'List_rate_type';
-    public const TABLE_EMPLOYEE_HIERARCHY = 'Employee_hierarchy';
+    public const TABLE_EMPLOYEES = 'employees';
+    public const TABLE_TEACHERS = 'teachers';
+    public const TABLE_LIST_DEGREE = 'list_degree';
+    public const TABLE_LIST_ACADEMIC_TITLE = 'list_academic_title';
+    public const TABLE_LIST_SCIENCE_TYPE = 'list_science_type';
+    public const TABLE_LIST_TEACHER_POST = 'list_teacher_post';
+    public const TABLE_LIST_EMPLOYEE_POST = 'list_employee_post';
+    public const TABLE_RATES = 'rates';
+    public const TABLE_LIST_RATE_TYPE = 'list_rate_type';
+    public const TABLE_EMPLOYEE_HIERARCHY = 'employee_hierarchy';
 
-    public const TABLE_EMPLOYEE_FILES = 'Employee_files';
-    public const TABLE_LIST_FILE_TAG = 'List_file_tag';
+    public const TABLE_EMPLOYEE_FILES = 'employee_files';
+    public const TABLE_LIST_FILE_TAG = 'list_file_tag';
 
-    public const TABLE_TICKETS = 'Tickets';
-    public const TABLE_LIST_TICKET_TYPE = 'List_ticket_type';
-    public const TABLE_EMPLOYEE_TICKETS = 'Employee_tickets';
-    public const TABLE_LIST_TICKET_STATUS = 'List_ticket_status';
-    public const TABLE_TICKET_FILES = 'Ticket_files';
-    public const TABLE_TICKET_HISTORY = 'Ticket_history';
-    public const TABLE_LIST_TICKET_HISTORY_TYPE = 'List_ticket_history_type';
+    public const TABLE_TICKETS = 'tickets';
+    public const TABLE_LIST_TICKET_TYPE = 'list_ticket_type';
+    public const TABLE_EMPLOYEE_TICKETS = 'employee_tickets';
+    public const TABLE_LIST_TICKET_STATUS = 'list_ticket_status';
+    public const TABLE_TICKET_FILES = 'ticket_files';
+    public const TABLE_TICKET_HISTORY = 'ticket_history';
+    public const TABLE_LIST_TICKET_HISTORY_TYPE = 'list_ticket_history_type';
 
 }
