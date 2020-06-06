3ee33b5494545ad8aa442afe36987933f70d529e
Sergey <pmswga@gmail.com>
2020-06-06 22:08:28 +0300

    Update ticket_show.blade.php

--- resources/views/systems/main/tickets/ticket_show.blade.php
+++ resources/views/systems/main/tickets/ticket_show.blade.php
@@ -67,7 +67,7 @@
                 @foreach($ticket->getAttachedFiles() as $file)
                     <div class="item">
                         <div class="right floated content">
-                            {{ $file->extension }} , {{ $file->size }} Мб
+                            {{ $file->getExtension() }} , {{ $file->getSize() }} Мб
                         </div>
                         <i class="file big icon"></i>
                         <div class="content">
@@ -93,7 +93,7 @@
                             {{ $employee->getFullInitials() }}
                         </div>
                         <div class="description">
-                            {{ $employee->getPost() }}
+                            {{ $employee->getPost()->getCaption() }}
                         </div>
                     </div>
                 </div>
